e222f0fc2993b392a39038134b25f76e4ac03532
Hasnake <haileyesus46@gmail.com>
2016-09-24 10:40:01 -0700

    Adding Js file

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 /////////////////////////////////////
 /* Problem 1 (this is your demo)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sum() that takes in two numbers as arguments and then returns
+//an array where the first element is the sum of those numbers, and the second element is a string that
+//EXACTLY follows this example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished,
+//uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
-// Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
+// Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops,
+//and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
-/////////////////////////////////////
+function sum(x,y{
+  var sum = x+y;
+  return [sum,'The sum of' +''+ x 'and'+ y is sum +'.']
+}
+sum(4,7);
+
+
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
